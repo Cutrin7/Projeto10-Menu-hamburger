6de88de37609757bce79e3cc54ec6ae47bd9e036
Vitor Cutrin <vitorcutrin@gmail.com>
2025-11-19 21:16:04 -0300

    feat: Projeto finalizado

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+
+
+    const mainNav = document.querySelector('.main-nav');
+
+ 
+    mobileMenuIcon.addEventListener('click', () => {
+
+        mainNav.classList.toggle('active');
+
+    });
+
+});
